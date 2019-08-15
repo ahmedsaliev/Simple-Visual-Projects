2be188e648ccc3cf0a8125afb0d091fb4f6e0194
ahmedsaliev <ahmed.saliev@gmail.com>
2019-08-15 20:34:39 +0300

    new content

---
+++ MovingButton/MovingButton/Form1.Designer.cs
@@ -0,0 +1,69 @@
+﻿namespace MovingButton
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.clickMeButton = new System.Windows.Forms.Button();
+            this.moveButtonTimer = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // clickMeButton
+            // 
+            this.clickMeButton.Location = new System.Drawing.Point(134, 127);
+            this.clickMeButton.Name = "clickMeButton";
+            this.clickMeButton.Size = new System.Drawing.Size(75, 23);
+            this.clickMeButton.TabIndex = 0;
+            this.clickMeButton.Text = "Start";
+            this.clickMeButton.UseVisualStyleBackColor = true;
+            this.clickMeButton.Click += new System.EventHandler(this.clickMeButton_Click);
+            // 
+            // moveButtonTimer
+            // 
+            this.moveButtonTimer.Interval = 800;
+            this.moveButtonTimer.Tick += new System.EventHandler(this.moveButtonTimer_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(292, 273);
+            this.Controls.Add(this.clickMeButton);
+            this.Name = "Form1";
+            this.Text = "Catch Me If You Can!";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button clickMeButton;
+        private System.Windows.Forms.Timer moveButtonTimer;
+    }
+}
+
---
+++ MovingButton/MovingButton/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MovingButton
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        Random Rand = new Random();
+
+        private void moveButtonTimer_Tick(object sender, EventArgs e)
+        {
+            clickMeButton.Left = Rand.Next(0, 250);
+            clickMeButton.Top = Rand.Next(0, 250);
+        }
+
+        private void clickMeButton_Click(object sender, EventArgs e)
+        {
+            moveButtonTimer.Enabled = !moveButtonTimer.Enabled;
+            clickMeButton.Text = "Catch Me!";
+        }
+    }
+}
